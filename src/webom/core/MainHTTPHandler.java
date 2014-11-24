7394e3fdb928c87425d2f7857fb53e5d13e2412d
Mustafa Akin <mustafa91@gmail.com>
2014-11-25 01:24:26 +0200

    Error Handling Works

--- src/webom/core/MainHTTPHandler.java
+++ src/webom/core/MainHTTPHandler.java
@@ -82,90 +82,87 @@ public class MainHTTPHandler extends AbstractHandler {
 				// Get the parameters in URL
 				Map<String, String> urlParams = route.matches(target);
 
-				try {
-					// TODO: Also build the session!!
-					Cookie[] cookies = request.getCookies();
-					String sessionKey = null;
-					if (cookies != null) {
-						for (Cookie cookie : cookies) {
-							if (cookie.getName().equals(SESSION_HEADER_NAME)) {
-								sessionKey = cookie.getValue();
-								break;
-							}
+				// TODO: Also build the session!!
+				Cookie[] cookies = request.getCookies();
+				String sessionKey = null;
+				if (cookies != null) {
+					for (Cookie cookie : cookies) {
+						if (cookie.getName().equals(SESSION_HEADER_NAME)) {
+							sessionKey = cookie.getValue();
+							break;
 						}
 					}
-
-					SessionBackend sessionBackend = webom.getSessionBackend();
-					Session session = sessionBackend.get(sessionKey);
-
-					if (session == null) {
-						session = new Session(sessionBackend);
-						sessionKey = session.getKey();
-						Cookie cookie = new Cookie(SESSION_HEADER_NAME, sessionKey);
-						response.addCookie(cookie);
-					}
-
-					// Instantiate the given class and build the object to
-					// handle it
-					HTTPRequestHandler instance = (HTTPRequestHandler) routeCls.newInstance();
-
-					// Generate our project specific Request/Response objects
-					webom.request.Request req = new webom.request.Request(request, session);
-					webom.response.Response res = new webom.response.Response(response);
-
-					// Build the POJO Class field values from either URL
-					// parameters,
-					// or GET/POST variables
-					instance.buildHTTP(urlParams, req.getRaw().getParameterMap(), session);
-
-					// Determine if the requirements are satisfied
-					boolean isValid = instance.isValid();
-					if (!isValid) {
-						res.getRaw().getWriter().println("The request is invalid:" + instance.getValidation_messages());
-						res.getRaw().setStatus(HTTPStatus.BAD_REQUEST);
-						// Don't attempt to handle the request
-					} else {
-						// Call the handle method of the newly instantiated
-						// class
-						Object result = instance.handle(req, res);
-
-						// Set the content type from given Response object,
-						// since
-						// the handle method could have changed them
-						response.setContentType(res.getContentType());
-						response.setStatus(res.getStatus());
-
-						// TODO: WHAT ABOUT CONTENT LENGTH? What is crucial?
-
-						// TODO: Gzip on large things
-
-						// Do the transmissions based on the returned object
-						// type
-						if (result instanceof String) {
-							stringTransformer.transform(req, res, (String) result);
-						} else if (result instanceof File) {
-							fileTransformer.transform(req, res, (File) result);
-						} else if (result instanceof InputStream) {
-
-						} else if (result instanceof Object) {
-							// WTF Then?
-						} else {
-							// Actually can never be here
-						}
-					}
-
-					// If it is not modified, it is up to SessionBackend to
-					// decide
-					// if actually update or leave it as it is
-					session.save();
-
-					long endTime = System.nanoTime();
-					double diff = (endTime / 1000000.0 - startTime / 1000000.0);
-					logger.info("{} {} {} {} ms", baseRequest.getMethod(), target, res.getRaw().getStatus(),
-							String.format("%.2f", diff));
-				} catch (Exception e) {
-					// Ignore for now
 				}
+
+				SessionBackend sessionBackend = webom.getSessionBackend();
+				Session session = sessionBackend.get(sessionKey);
+
+				if (session == null) {
+					session = new Session(sessionBackend);
+					sessionKey = session.getKey();
+					Cookie cookie = new Cookie(SESSION_HEADER_NAME, sessionKey);
+					response.addCookie(cookie);
+				}
+
+				// Instantiate the given class and build the object to
+				// handle it
+				HTTPRequestHandler instance = (HTTPRequestHandler) routeCls.newInstance();
+
+				// Generate our project specific Request/Response objects
+				webom.request.Request req = new webom.request.Request(request, session);
+				webom.response.Response res = new webom.response.Response(response);
+
+				// Build the POJO Class field values from either URL
+				// parameters,
+				// or GET/POST variables
+				instance.buildHTTP(urlParams, req.getRaw().getParameterMap(), session);
+
+				// Determine if the requirements are satisfied
+				boolean isValid = instance.isValid();
+				if (!isValid) {
+					res.getRaw().getWriter().println("The request is invalid:" + instance.getValidation_messages());
+					res.getRaw().setStatus(HTTPStatus.BAD_REQUEST);
+					// Don't attempt to handle the request
+				} else {
+					// Call the handle method of the newly instantiated
+					// class
+					Object result = instance.handle(req, res);
+
+					// Set the content type from given Response object,
+					// since
+					// the handle method could have changed them
+					response.setContentType(res.getContentType());
+					response.setStatus(res.getStatus());
+
+					// TODO: WHAT ABOUT CONTENT LENGTH? What is crucial?
+
+					// TODO: Gzip on large things
+
+					// Do the transmissions based on the returned object
+					// type
+					if (result instanceof String) {
+						stringTransformer.transform(req, res, (String) result);
+					} else if (result instanceof File) {
+						fileTransformer.transform(req, res, (File) result);
+					} else if (result instanceof InputStream) {
+
+					} else if (result instanceof Object) {
+						// WTF Then?
+					} else {
+						// Actually can never be here
+					}
+				}
+
+				// If it is not modified, it is up to SessionBackend to
+				// decide
+				// if actually update or leave it as it is
+				session.save();
+
+				long endTime = System.nanoTime();
+				double diff = (endTime / 1000000.0 - startTime / 1000000.0);
+				logger.info("{} {} {} {} ms", baseRequest.getMethod(), target, res.getRaw().getStatus(),
+						String.format("%.2f", diff));
+
 			}
 			baseRequest.setHandled(true);
 		} catch (Exception ex) {
@@ -177,14 +174,14 @@ public class MainHTTPHandler extends AbstractHandler {
 			} else {
 				output.println("An exception has occurred: \n");
 				ex.printStackTrace(output);
-				output.println("\n\n ** You can disable this message by setting the WebOM in production mode.");				
+				output.println("\n\n ** You can disable this message by setting the WebOM in production mode.");
 			}
 
 			long endTime = System.nanoTime();
 			double diff = (endTime / 1000000.0 - startTime / 1000000.0);
 			// Better error reporting
-			logger.info("{} {} {} {} ms: {}", baseRequest.getMethod(), target, HTTPStatus.INTERNAL_SERVER_ERROR,
-					String.format("%.2f", diff, ex.getMessage()));
+			logger.info("{} {} {} {} ms #Error: {}", baseRequest.getMethod(), target, HTTPStatus.INTERNAL_SERVER_ERROR,
+					String.format("%.2f", diff), ex.getMessage());
 			baseRequest.setHandled(true);
 		}
 	}
