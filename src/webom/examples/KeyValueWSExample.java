0638a2cbd99076bc79a1846db6e60271b42cc4e3
Mustafa Akin <mustafa91@gmail.com>
2015-01-23 06:13:03 +0200

    Key-value based websocket messaging with POJO building

---
+++ src/webom/examples/KeyValueWSExample.java
@@ -0,0 +1,56 @@
+package webom.examples;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import webom.annotations.request.Handler;
+import webom.annotations.request.RequestMethod;
+import webom.annotations.request.WebSocketKey;
+import webom.request.KeyValueWebSocketHandler;
+
+@Handler(method = RequestMethod.WEBSOCKET, path = "/keyvalue")
+public class KeyValueWSExample extends KeyValueWebSocketHandler{
+
+	private static Logger logger = LoggerFactory.getLogger(KeyValueWSExample.class);
+
+	@Override
+	public void onWebSocketBinary(byte[] payload, int offset, int len) {
+
+	}
+
+	@Override
+	public void onWebSocketClose(int statusCode, String reason) {
+		logger.info("Web socket closed, code: {} reason: {}", statusCode, reason);
+	}
+
+	@Override
+	public void onWebSocketConnect() {
+		logger.info("Websocket connected");
+		super.onWebSocketConnect();
+	}
+
+	@Override
+	public void onWebSocketError(Throwable cause) {
+		cause.printStackTrace();
+	}
+
+	@Override
+	public void onUnknownWebSocketText(String message) {
+		logger.info("Unknown message recieved");
+	}
+	
+	public static class Person {
+		int age;
+		String name;
+		@Override
+		public String toString() {
+			return "Hi i am " + name + " and " + age + " years old";
+		}
+	}
+	
+	@WebSocketKey(key="person")
+	public void onPerson(Person person){
+		logger.info("Person recieved and it says " + person);
+	}
+	
+}
---
+++ src/webom/request/KeyValueWebSocketHandler.java
@@ -0,0 +1,64 @@
+package webom.request;
+
+import java.lang.reflect.Method;
+import java.util.HashMap;
+
+import webom.annotations.request.WebSocketKey;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+public abstract class KeyValueWebSocketHandler extends WebSocketHandler {
+	private HashMap<String, Method> mappedMethods = new HashMap<>();
+
+	@Override
+	public void onWebSocketConnect() {
+		// Register the functions once
+		Class<? extends KeyValueWebSocketHandler> cls = this.getClass();
+		Method[] methods = cls.getMethods();
+		for (Method method : methods) {
+			WebSocketKey wsk = method.getAnnotation(WebSocketKey.class);
+			if (wsk != null) {
+				String key = wsk.key();
+				mappedMethods.put(key, method);
+			}
+		}
+	}
+
+	@Override
+	public void onWebSocketError(Throwable cause) {
+
+	}
+
+	@Override
+	public void onWebSocketText(String message) {
+		// Parse the JSON message
+		try {
+			JsonObject json = new JsonParser().parse(message).getAsJsonObject();
+			String key = json.get("key").getAsString();
+			
+			Method method = mappedMethods.get(key);
+			if (method == null) {
+				onUnknownWebSocketText(message);
+				return;
+			} else {
+				JsonElement value = json.get("value");
+				Gson gson = new Gson();
+				Class<?> cls = method.getParameterTypes()[0]; 
+				// First parameter matters only, must be ensured in onWebSOcketConnect while constructing mappedMethods
+				Object valueObject = gson.fromJson(value, cls);
+				method.invoke(this, valueObject);
+			}
+
+		} catch (Exception ex) {
+			// Invalid json
+			onUnknownWebSocketText(message);
+			ex.printStackTrace();
+		}
+	}
+
+	public abstract void onUnknownWebSocketText(String message);
+
+}
